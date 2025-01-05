7bd7cbd8e06c1ddad19429920006c83ecb1c62b5
Le Hoang Nam <lehoangnam040@gmail.com>
2025-01-05 21:23:45 +0700

    code

--- kasmvnc-docker-sysbox-runc/root/start.sh
+++ kasmvnc-docker-sysbox-runc/root/start.sh
@@ -3,13 +3,12 @@
 sudo chown -R 1000:1000 /defaults
 sudo chown -R 1000:1000 /etc/s6-overlay/
 
-sudo bash /etc/s6-overlay/s6-rc.d/init-nginx/run
 sudo bash /etc/s6-overlay/s6-rc.d/init-video/run
 sudo bash /etc/s6-overlay/s6-rc.d/init-kasmvnc-config/run
 
 bash /etc/s6-overlay/s6-rc.d/svc-kasmvnc/run &
 bash /etc/s6-overlay/s6-rc.d/svc-de/run &
 
-service nginx reload
+sudo service nginx reload
 cd /kclient
 node index.js
